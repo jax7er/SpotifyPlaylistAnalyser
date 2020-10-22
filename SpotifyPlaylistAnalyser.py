2b6b7d48c2a64f460641a1ab4a06468df1bd34d9
Jack <jax7er@gmail.com>
2020-10-22 11:41:38 +0100

    Update SpotifyPlaylistAnalyser.py

--- SpotifyPlaylistAnalyser.py
+++ SpotifyPlaylistAnalyser.py
@@ -1,8 +1,8 @@
 import string
 from collections import Counter
 from os import getenv
+from typing import List
 
-import spotipy.util as util
 from spotipy import Spotify
 from spotipy.exceptions import SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -20,64 +20,86 @@ spotify = Spotify(
 )
 
 
-def get_top_artists(tracks: dict) -> (list, int):
-    count = Counter()
+def get_top_artists(tracks: dict) -> (List[str], int):
+    artist_count = Counter()
 
     while tracks is not None:
-        count.update(
+        artist_count.update(
             artist["name"].strip() 
             for track in tracks["items"]
+            if track["track"] is not None
             for artist in track["track"]["artists"]
         )
 
         tracks = spotify.next(tracks)
 
-    top_count = max(count.values())
+    if not artist_count:
+        return [], 0
+
+    top_count = max(artist_count.values())
 
     top_artists = [
         name
-        for name, count in count.items()
+        for name, count in artist_count.items()
         if count == top_count
     ]
 
-    return top_artists, top_count
+    if len(top_artists) == len(artist_count):
+        return ["All artists have same track count"], top_count
+    else:
+        return top_artists, top_count
 
 
-def get_duplicates(tracks: dict) -> list:
+def get_duplicates(tracks: dict) -> List[dict]:
     duplicate_meta = {}
-    all_meta_processed = []
+    all_meta_processed = set()
 
     while tracks is not None:
         for track in tracks["items"]:
-            name = track["track"]["name"]
-            artists = ", ".join(artist["name"].strip() for artist in track["track"]["artists"])
-            album = track["track"]["album"]["name"]
+            if track["track"] is None:
+                continue
 
-            meta_processed = "".join(
-                c.lower()
-                for c in (name + artists + album)
-                if c.isalnum()
+            name = track["track"]["name"]
+            album = track["track"]["album"]["name"]
+            artists_gen = (
+                artist["name"].strip()
+                for artist in track["track"]["artists"]
             )
+            artists = ", ".join(sorted(artists_gen, key=str.lower))
+
+            meta_filtered = filter(str.isalnum, name + artists + album)
+            meta_processed = "".join(meta_filtered).lower()
 
             if meta_processed in all_meta_processed:
                 if meta_processed in duplicate_meta:
                     duplicate_meta[meta_processed]["count"] += 1
                 else:
-                    duplicate_meta[meta_processed] = {"name": name, "artists": artists, "album": album, "count": 2}
-
-            all_meta_processed.append(meta_processed)
+                    duplicate_meta[meta_processed] = {
+                        "name": name, 
+                        "artists": artists,
+                        "album": album, 
+                        "count": 2
+                    }
+            else:
+                all_meta_processed.add(meta_processed)
 
         tracks = spotify.next(tracks)
 
     return list(duplicate_meta.values())
 
 
-def print_results(playlist_name, top_artists, top_count, duplicates):
-    print(f"\t{playlist_name}")
+def print_results(
+    playlist_name: str, 
+    num_tracks: int, 
+    top_artists: List[str], 
+    top_count: int, 
+    duplicates: List[dict]
+):
+    print(f"\t{playlist_name} - {num_tracks} tracks")
 
-    print(f"\t\tTop artist(s): \"", end="")
+    print(f"\t\tTop artist(s): ", end="")
     print(*top_artists, sep=", ", end="")
-    print(f"\", track count: {top_count}")
+    print(f" with {top_count} track(s) (each)")
     
     if not duplicates:
         print(f"\t\tNo duplicate tracks")
@@ -85,10 +107,13 @@ def print_results(playlist_name, top_artists, top_count, duplicates):
         print(f"\t\t{len(duplicates)} track(s) with duplicates")
 
         for duplicate in duplicates:
-            print("\t\t\t\"{name}\" by "
-                    "\"{artists}\" on "
-                    "\"{album}\" repeats "
-                    "{count} times".format(**duplicate))
+            print(
+                "\t\t\t"
+                "\"{name}\" by "
+                "\"{artists}\" on "
+                "\"{album}\" appears "
+                "{count} times".format(**duplicate)
+            )
 
 
 def analyse(username: str):
@@ -97,18 +122,34 @@ def analyse(username: str):
     except SpotifyException:
         playlists = None
     
+    if playlists is None:
+        print(f"User \"{username}\" is invalid or has no playlists")
+    else:
+        print(f"{username} - owns/follows {playlists['total']} playlist(s)")
+    
     while playlists is not None:
         for playlist in playlists["items"]:
-            if playlist["owner"]["id"] != username:
-                continue
+            # ensure the playlist was created by the user
+            # if playlist["owner"]["id"] != username:
+            #     continue
             
             tracks = spotify.playlist_tracks(playlist["id"])
 
+            # ensure the playlist has at least one track
+            if tracks is None:
+                continue
+            
             top_artists, top_count = get_top_artists(tracks)
 
             duplicates = get_duplicates(tracks)
 
-            print_results(playlist["name"], top_artists, top_count, duplicates)
+            print_results(
+                playlist["name"], 
+                tracks["total"], 
+                top_artists, 
+                top_count, 
+                duplicates
+            )
 
         playlists = spotify.next(playlists)
 
